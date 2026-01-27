387442e184c7deff52acd18bc70984e3d1d08589
Madhav Raninga <madhavraninga3@gmail.com>
2026-01-27 15:25:36 +0530

    madhav

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://e-commerce-frontend-3cj.pages.dev"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
