19ad30f09601612ef9e718d8870a741a3a5fdcd9
unknown <madhavraninga3@gmail.com>
2026-02-02 21:56:05 +0530

    commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,23 +1,23 @@
-const jwt = require("jsonwebtoken")
-const dotenv = require("dotenv")
-dotenv.config()
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 exports.generateToken = (id, res) => {
   const token = jwt.sign(
     { id },
     process.env.JWT_SECRET_KEY,
     { expiresIn: process.env.JWT_EXPIRE }
-  )
+  );
 
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true, 
-    sameSite: "None"
-  }
+    secure: true,      // REQUIRED (HTTPS)
+    sameSite: "None",  // ✅ MUST be capital N
+  };
 
-  res.cookie("token", token, options)
-  return token
-}
+  res.cookie("token", token, options);
+  return token;
+};
